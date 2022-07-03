181dc5ee6b6f49d65690c6a5eca88be2dc8beb28
Pablo Daniel Álvarez Graña <pablo.alvarez4284@gmail.com>
2022-07-03 18:34:57 -0300

    CB (PJS) - Ver. 2.1 - Agregado de apartado 'Cuenta'

--- js/scripts.js
+++ js/scripts.js
@@ -11,25 +11,25 @@
 
 // ARRAY PRODUCTOS
 const products = [
-    {id:1, title:'Funko Pop de Goku', price: 1500, stock: true, category: 'figure'},
-    {id:2, title:'Carta de Charizard', price: 2000, stock: true, category: 'card'},
-    {id:3, title:'Carta de Pikachu', price: 500, stock: false, category: 'card'},
-    {id:4, title:'Figura a escala de Broly', price: 4000, stock: true, category: 'figure'}
+    { id: 1, title: 'Funko Pop de Goku', price: 1500, stock: true, category: 'figure' },
+    { id: 2, title: 'Carta de Charizard', price: 2000, stock: true, category: 'card' },
+    { id: 3, title: 'Carta de Pikachu', price: 500, stock: false, category: 'card' },
+    { id: 4, title: 'Figura a escala de Broly', price: 4000, stock: true, category: 'figure' }
 ];
 
 const cartStorage = localStorage.getItem('cart');
 
 const cart = JSON.parse(cartStorage) ?? [];
 
-const cartTotalAmountStart = cart.reduce ((sum, product) => sum + product.price, 0);
+const cartTotalAmountStart = cart.reduce((sum, product) => sum + product.price, 0);
 
 document.getElementById('cart-string').innerHTML = `${cart.length} - $${cartTotalAmountStart}`;
 
 // GENERADOR CARDS
 const generateCards = (arrayFiltered) => {
-let generatorCards = ``;
-arrayFiltered.forEach((product) => {
-    generatorCards +=`    <div class="col mb-5">
+    let generatorCards = ``;
+    arrayFiltered.forEach((product) => {
+        generatorCards += `    <div class="col mb-5">
     <div class="card h-100">
         <!-- Product image-->
         <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
@@ -49,17 +49,17 @@ arrayFiltered.forEach((product) => {
         </div>
     </div>
 </div>`;
-})
-document.getElementById('container-products').innerHTML = generatorCards;
+    })
+    document.getElementById('container-products').innerHTML = generatorCards;
 };
 generateCards(products);
 
 // FUNCION AGREGAR UN product AL CARRTTO
-const addToCart = (productId) =>{
+const addToCart = (productId) => {
     const foundIndex = products.findIndex(product => product.id == productId);
     if (products[foundIndex].stock == true) {
         cart.push(products[foundIndex]);
-        const cartTotalAmount = cart.reduce ((sum, product) => sum + product.price, 0);
+        const cartTotalAmount = cart.reduce((sum, product) => sum + product.price, 0);
         document.getElementById('cart-string').innerHTML = `${cart.length} - $${cartTotalAmount}`
         const cartJSON = JSON.stringify(cart)
         localStorage.setItem('cart', cartJSON);
@@ -73,8 +73,8 @@ const addToCart = (productId) =>{
 const removeFromCart = (cartProductId) => {
     const foundIndexCart = cart.findIndex(cartProduct => cartProduct.id == cartProductId);
     if (foundIndexCart != -1) {
-        cart.splice(foundIndexCart,1);
-        const cartTotalAmount = cart.reduce ((sum, product) => sum + product.price, 0);
+        cart.splice(foundIndexCart, 1);
+        const cartTotalAmount = cart.reduce((sum, product) => sum + product.price, 0);
         document.getElementById('cart-string').innerHTML = `${cart.length} - $${cartTotalAmount}`
         const cartJSON = JSON.stringify(cart)
         localStorage.setItem('cart', cartJSON);
@@ -101,12 +101,12 @@ const removeFromCart = (cartProductId) => {
 // FUNCION MOSTRAR PRODUCTOS EN EL CARRITO
 const btnShowCart = document.getElementById('btn_show-cart')
 btnShowCart.onclick = () => {
-    console.log ('Estos son los productos que tenés en el carrito:');
+    console.log('Estos son los productos que tenés en el carrito:');
     cart.forEach((product) => {
         console.log(`${product.title} - $${product.price}`)
     })
-    const cartTotalAmount = cart.reduce ((sum, product) => sum + product.price, 0)
-    console.log (`El total de tu carrito es de $${cartTotalAmount}`)
+    const cartTotalAmount = cart.reduce((sum, product) => sum + product.price, 0)
+    console.log(`El total de tu carrito es de $${cartTotalAmount}`)
 };
 
 /* FUNCION FILTRAR POR CATEGORIAS */
@@ -116,13 +116,13 @@ const filterByCategory = (category) => {
 }
 
 const categoryAll = document.getElementById('category_all')
-categoryAll.onclick = () => {generateCards(products)}
+categoryAll.onclick = () => { generateCards(products) }
 
 const categoryCards = document.getElementById('category_cards')
-categoryCards.onclick = () => {filterByCategory('card')}
+categoryCards.onclick = () => { filterByCategory('card') }
 
 const categoryFigures = document.getElementById('category_figures')
-categoryFigures.onclick = () => {filterByCategory('figure')}
+categoryFigures.onclick = () => { filterByCategory('figure') }
 
 const categoryInStock = document.getElementById('category_in-stock')
 categoryInStock.onclick = () => {
@@ -130,7 +130,64 @@ categoryInStock.onclick = () => {
     generateCards(stockTrueArray)
 }
 
-const botonPrueba = document.getElementById('boton_prueba')
-botonPrueba.onclick = () => {
-    document.getElementById('modal_prueba')
+/* FUNCION VALIDACION DE CUENTA */
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
+const buttonEnableValidation = () => {
+    const accountEmailError = document.getElementById('emailError').innerHTML
+    const accountPasswordError = document.getElementById('passwordError').innerHTML
+    if (accountEmailError + accountPasswordError == '') {
+        buttonValidate.disabled = false;
+    } else {
+        buttonValidate.disabled = true;
+    }
+}
+
+document.getElementById('accountEmail').onchange = (e) => {
+    const accountEmail = e.target.value
+    const isAccountEmailValid = validateEmail(accountEmail);
+    if (isAccountEmailValid) {
+        document.getElementById('emailError').innerHTML = ``
+    } else {
+        document.getElementById('emailError').innerHTML = `<p class="error_login">El correo electrónico es inválido.</p>`
+    }
+    buttonEnableValidation ();
+}
+
+document.getElementById('accountPassword').onchange = (e) => {
+    const accountPassword = e.target.value
+    if (accountPassword.length >= 8) {
+        document.getElementById('passwordError').innerHTML = ``
+    } else {
+        document.getElementById('passwordError').innerHTML = `<p class="error_login">La contraseña debe contener 8 caracteres (mín).</p>`
+    }
+    buttonEnableValidation ();
+}
+
+const accountValidations = () => {
+    const accountEmail = document.getElementById('accountEmail').value
+    const accountPassword = document.getElementById('accountPassword').value
+    const isAccountEmailValid = validateEmail(accountEmail)
+    if (isAccountEmailValid && accountPassword.length >= 8) {
+        document.getElementById('emailError').innerHTML = ``
+        document.getElementById('passwordError').innerHTML = ``
+        alert(`Se ha iniciado sesión con el correo "${accountEmail}" exitosamente.`)
+        document.getElementById('accountModalLabel').innerHTML = 'Datos de la sesión'
+        document.getElementById('accountModalBody').innerHTML = `<p>Sesión iniciada con el correo "${accountEmail}"</p>`
+    } else if (isAccountEmailValid == false) {
+        document.getElementById('passwordError').innerHTML = `<p class="error_login">La contraseña debe contener 8 caracteres (mín).</p>`
+    } else if (accountPassword.length < 8) {
+        document.getElementById('emailError').innerHTML = `<p class="error_login">El correo electrónico es inválido.</p>`
+    }
+}
+
+const buttonValidate = document.getElementById('buttonValidate')
+buttonValidate.onclick = () => {
+    accountValidations();
 }
